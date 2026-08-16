1ff1c90ef1f2140d15d7cca64730c7871b925a31
lielbs <lielbs@users.noreply.github.com>
2026-08-16 16:53:08 +0300

    Updates

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'counter-shell-v1'
+const SHELL = ['./', './index.html', './manifest.json', './icons/icon.svg', './icons/icon-maskable.svg']
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()))
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()))
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone()
+    caches.open(CACHE).then(cache => cache.put(event.request, copy))
+    return response
+  }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html'))))
+})
